237a4e16379d6cfca95fe6ccbae0971e0c3ec73d
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-06-17 15:46:38 -0600

    Create Category model and install Entity Framework

---
+++ BulkyWeb/BulkyWeb/README.md
@@ -0,0 +1,6 @@
+Add connectionstrings within appsettings.json for SQL connection
+
+Install Microsoft.EntityFrameworkCore - "dotnet add package Microsoft.EntityFrameworkCore"
+Install Microsoft.EntityFrameworkCore.SQLServer - "dotnet add package Microsoft.EntityFrameworkCore.SqlServer"
+Install Microsoft.EntityFrameworkCore.Tools - "dotnet tool install --global dotnet-ef"
+Install Microsoft.EntityFrameworkCore.Design - "dotnet add package Microsoft.EntityFrameworkCore.Design"
--- BulkyWeb/BulkyWeb/obj/Debug/net9.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/BulkyWeb/obj/Debug/net9.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffbd961a1efcf4452c8a880963103f7a6c89e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729cbaa11e87c488f818f511bb886c34337ed745")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
